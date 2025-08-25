16c46bbd78aa41a18cef30664f6e6c5a01416890
jason <jason@JH-OMEN>
2025-08-25 15:06:56 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Please enter a number?");
+int numberOne = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Please enter a second number");
+int numberTwo = Convert.ToInt32(Console.ReadLine());
+int result = numberOne + numberTwo;
+Console.WriteLine($"{numberOne} + {numberTwo} = {result}");
